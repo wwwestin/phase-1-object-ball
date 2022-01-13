e37cf1943014429df901412b7f0e6288da4d97a7
Westin Humble <westin.h@gmail.com>
2022-01-13 10:12:26 -0500

    switching drivers

---
+++ src/00-objectball.js
@@ -0,0 +1,31 @@
+function gameObject() {
+    {home: 
+        {teamname: "Brooklyn",
+         colors: "Black, White",
+         players: {number: 0 
+                   shoe: 16,
+                   points: 22,
+                   rebounds: 12,
+                   asssists: 12,
+                   steals: 3,
+                   blocks: 1,
+                   slamdunks: 1,
+                  }
+        
+    }
+    }
+    
+        Away: {
+                teamname:
+                colors:
+                players: {number:
+                          shoe:
+                          points:
+                          rebounds:
+                          asssists:
+                          steals:
+                          blocks:
+                          slamdunks: 
+                         }
+  } 
+};
