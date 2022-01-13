c3243eb08c2a720c4694f828d4cf88d67b3c40e2
Jonathan Bryant <spectrum19tech@gmail.com>
2022-01-13 08:05:16 -0800

    Changing driver

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,31 +1,118 @@
 function gameObject() {
-    {home: 
-        {teamname: "Brooklyn",
-         colors: "Black, White",
-         players: {number: 0 
-                   shoe: 16,
-                   points: 22,
-                   rebounds: 12,
-                   asssists: 12,
-                   steals: 3,
-                   blocks: 1,
-                   slamdunks: 1,
-                  }
-        
-    }
-    }
-    
-        Away: {
-                teamname:
-                colors:
-                players: {number:
-                          shoe:
-                          points:
-                          rebounds:
-                          asssists:
-                          steals:
-                          blocks:
-                          slamdunks: 
-                         }
-  } 
-};
+    return {home: 
+            {teamname: "Brooklyn Nets",
+            colors: ["Black, White"],
+            players: {
+                [{playerName: 'Alan Anderson',
+                    number: 0 ,
+                    shoe: 16,
+                    points: 22,
+                    rebounds: 12,
+                    asssists: 12,
+                    steals: 3,
+                    blocks: 1,
+                    slamdunks: 1
+                    },
+                    {
+                    playerName: 'Reggie Evans',
+                    number: 30,
+                    shoe: 14,
+                    points: 12,
+                    rebounds: 12,
+                    asssists: 12,
+                    steals: 12,
+                    blocks: 12,
+                    slamdunks: 7
+                    },
+                    {
+                    playerName: 'Brook Lopez',
+                    number: 11,
+                    shoe: 17,
+                    points: 17,
+                    rebounds: 19,
+                    asssists: 10,
+                    steals: 3,
+                    blocks: 1,
+                    slamdunks: 15
+                    },
+                    {
+                    playerName: 'Mason Plumlee',
+                    number: 1,
+                    shoe: 19,
+                    points: 26,
+                    rebounds: 12,
+                    asssists: 6,
+                    steals: 3,
+                    blocks: 8,
+                    slamdunks: 5
+                    },
+                    {
+                    playerName: 'Jason Terry',
+                    number: 31,
+                    shoe: 15,
+                    points: 19,
+                    rebounds: 2,
+                    asssists: 2,
+                    steals: 4,
+                    blocks: 11,
+                    slamdunks: 1
+                    }]}},
+            
+            away: 
+            {teamname: "Charlotte Hornets",
+            colors: ["Turquoise, Purple"],
+            players: {
+                [{playerName: 'Jeff Adrien',
+                    number: 4,
+                    shoe: 18,
+                    points: 10,
+                    rebounds: 1,
+                    asssists: 1,
+                    steals: 2,
+                    blocks: 7,
+                    slamdunks: 2
+                    },
+                    {
+                    playerName: 'Bismak Biyombo',
+                    number: 0,
+                    shoe: 16,
+                    points: 4,
+                    rebounds: 7,
+                    asssists: 7,
+                    steals: 15,
+                    blocks: 10,
+                    slamdunks: 10
+                    },
+                    {
+                    playerName: 'DeSagna Diop',
+                    number: 2,
+                    shoe: 14,
+                    points: 24,
+                    rebounds: 12,
+                    asssists: 12,
+                    steals: 4,
+                    blocks: 5,
+                    slamdunks: 5
+                    },
+                    {
+                    playerName: 'Ben Gordon',
+                    number: 8,
+                    shoe: 15,
+                    points: 33,
+                    rebounds: 3,
+                    asssists: 2,
+                    steals: 1,
+                    blocks: 1,
+                    slamdunks: 0
+                    },
+                    {
+                    playerName: 'Brendan Haywood',
+                    number: 33,
+                    shoe: 15,
+                    points: 6,
+                    rebounds: 12,
+                    asssists: 12,
+                    steals: 22,
+                    blocks: 5,
+                    slamdunks: 12
+                    }]}},
